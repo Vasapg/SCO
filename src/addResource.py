4385359d0eba2ff450a2760a4fbe436e62efed3e
Victor Sanz Ararat <victoralejandro.sanz@alumnos.upm.es>
2024-03-06 22:19:32 +0100

    New: allow several files to resource

--- src/addResource.py
+++ src/addResource.py
@@ -15,7 +15,7 @@ def prettify_xml(elem):
             lines.append(node.toxml())
     return "\n".join(lines)
 
-def add_resource_to_imsmanifest(file_path):
+def add_resource_to_imsmanifest(files):
     # Buscar el archivo imsmanifest.xml en el directorio actual
     current_directory = os.getcwd()
     xml_file = os.path.join(current_directory, "imsmanifest.xml")
@@ -29,35 +29,58 @@ def add_resource_to_imsmanifest(file_path):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    # Agregar un nuevo recurso
+    # Encontrar o crear el elemento de recursos
     resources = root.find("./{http://www.imsglobal.org/xsd/imscp_v1p1}resources")
-    resource = ET.SubElement(resources, "{http://www.imsglobal.org/xsd/imscp_v1p1}resource")
-    resource.set("identifier", os.path.basename(file_path))
-    resource.set("type", "webcontent")
-    resource.set("{http://www.adlnet.org/xsd/adlcp_v1p3}scormType", "sco")
-    resource.set("href", os.path.basename(file_path))
+    if resources is None:
+        resources = ET.SubElement(root, "{http://www.imsglobal.org/xsd/imscp_v1p1}resources")
 
-    file = ET.SubElement(resource, "{http://www.imsglobal.org/xsd/imscp_v1p1}file")
-    file.set("href", os.path.basename(file_path))
+    # Verificar si el resource ya existe
+    existing_resource = resources.find(f"./{{http://www.imsglobal.org/xsd/imscp_v1p1}}resource[@identifier='{os.path.basename(files[0])}']")
+    if existing_resource is not None:
+        # El resource ya existe, agregar solo los archivos que faltan
+        for file_path in files:
+            file_name = os.path.basename(file_path)
+            existing_file = existing_resource.find(f"./{http://www.imsglobal.org/xsd/imscp_v1p1}file[@href='{file_name}']")
+            if existing_file is None:
+                file_element = ET.SubElement(existing_resource, "{http://www.imsglobal.org/xsd/imscp_v1p1}file")
+                file_element.set("href", file_name)
+                file_element.tail = '\n'  # Agregar salto de línea después de la etiqueta <file>
+        existing_resource.tail = '\n'  # Agregar salto de línea después de la etiqueta <resource>
+    else:
+        # El resource no existe, crear uno nuevo y agregar archivos
+        new_resource = ET.SubElement(resources, "{http://www.imsglobal.org/xsd/imscp_v1p1}resource")
+        new_resource.set("identifier", os.path.basename(files[0]))
+        new_resource.set("type", "webcontent")
+        new_resource.set("{http://www.adlnet.org/xsd/adlcp_v1p3}scormType", "sco")
+        for file_path in files:
+            if not os.path.exists(file_path):
+                print("El archivo " + file_path + " no existe")
+                sys.exit(1)
+            file_name = os.path.basename(file_path)
+            file_element = ET.SubElement(new_resource, "{http://www.imsglobal.org/xsd/imscp_v1p1}file")
+            file_element.set("href", file_name)
+            file_element.tail = '\n'  # Agregar salto de línea después de la etiqueta <file>
+        new_resource.tail = '\n'  # Agregar salto de línea después de la etiqueta <resource>
 
     # Escribir el árbol XML en el archivo imsmanifest.xml con formato legible
     with open(xml_file, "wb") as f:
         f.write(prettify_xml(root).encode('utf-8'))
 
-    print(f"Recurso {file_path} añadido al archivo imsmanifest.xml.")
+    print(f"Archivos {', '.join(files)} añadidos al archivo imsmanifest.xml.")
 
 if __name__ == "__main__":
-    # Verificar que se pasó el argumento correctamente
-    if len(sys.argv) != 2:
-        print("Uso: python script.py <archivo>")
+    # Verificar que se pasó al menos un argumento
+    if len(sys.argv) < 2:
+        print("Uso: python script.py <archivo1> <archivo2> ...")
         sys.exit(1)
 
-    file_path = sys.argv[1]
+    files = sys.argv[1:]
 
-    # Verificar si el archivo a agregar como recurso existe
-    if not os.path.exists(file_path):
-        print("El archivo a agregar como recurso no existe.")
-        sys.exit(1)
+    # Verificar si los archivos existen
+    for file_path in files:
+        if not os.path.exists(file_path):
+            print(f"El archivo {file_path} no existe.")
+            sys.exit(1)
 
-    # Agregar el recurso al imsmanifest.xml
-    add_resource_to_imsmanifest(file_path)
+    # Agregar los archivos al imsmanifest.xml
+    add_resource_to_imsmanifest(files)
