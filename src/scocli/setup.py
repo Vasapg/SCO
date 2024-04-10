7da8dc1ff95b48da9803ff5e338f353a6f0fb848
Victor Sanz Ararat <victoralejandro.sanz@alumnos.upm.es>
2024-04-10 13:57:46 +0200

    fix: simplify setup.py

--- src/scocli/setup.py
+++ src/scocli/setup.py
@@ -4,15 +4,12 @@ setup(
     name='scocli',
     version='0.1',
     author= 'Vasapg',
-    description= 'CLI to facilitate the creation and managing of SCORM packages',
+    description= 'CLI to facilitate the creation and management of SCORM packages',
     url='https://github.com/Vasapg/SCO/tree/main',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'scoaddResource = sco.addResource:main',
-            'scoremoveResource = sco.removeResource:main',
-            'scoseeManifest = sco.seeManifest:main',
-            'scobuild = sco.build:main',
+    scripts=[
+        'sco/removeResource.py',
+        'sco/addResource.py',
+        'sco/build.py',
+        'sco/seeManifest.py',
         ],
-    }
 )
