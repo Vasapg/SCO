d64e4df8d255a4474038354948c12a473a8d81e0
Victor Sanz Ararat <victoralejandro.sanz@alumnos.upm.es>
2024-05-18 18:10:04 +0200

    Fix: checker not verifying <files> elements

--- src/scocli/sco/checkManifest.py
+++ src/scocli/sco/checkManifest.py
@@ -5,14 +5,23 @@ from xml.etree import ElementTree as ET
 def recopilar_archivos(ruta, recursos):
     archivos = []
     archivos_no_utilizados = []
+    # Files to ignore
+    ignore_files = ["package-lock.json", "package.json", "imsmanifest.xml", "node_modules/.package-lock.json", "node_modules/scoapi/scoAPI.js", "node_modules/scoapi/package.json"]
+    # Extensions to ignore
+    ignore_extensions = [".zip", ".babelrc"]
+
     for root, dirs, files in os.walk(ruta):
         for file in files:
             archivo = os.path.relpath(os.path.join(root, file), ruta)
             archivos.append(archivo)
+            # Check if the file is in the ignore list
+            if archivo in ignore_files or file.endswith(tuple(ignore_extensions)):
+                continue
             # Comprobar si el archivo no está en los recursos definidos
             if archivo not in recursos:
                 archivos_no_utilizados.append(archivo)
     return archivos, archivos_no_utilizados
+
 
 def validar_archivos_necesarios(raiz_proyecto):
     # Lista de archivos necesarios en un proyecto SCORM
@@ -40,26 +49,32 @@ def validar_imsmanifest(ruta_imsmanifest):
             'ns2': 'http://www.adlnet.org/xsd/adlcp_v1p3'
         }
 
-        # Obtener todos los identifiers de los recursos definidos en imsmanifest.xml
-        recursos = {resource.attrib['identifier'] for resource in root.findall(".//ims:resource", namespaces=ns)}
+        # Obtener todos los archivos asociados a los recursos definidos en imsmanifest.xml
+        recursos = set()
+        files = set()
+        for resource in root.findall(".//{http://www.imsglobal.org/xsd/imscp_v1p1}resource", namespaces=ns):
+            identifier = resource.attrib['identifier']
+            for file_elem in resource.findall(".//{http://www.imsglobal.org/xsd/imscp_v1p1}file", namespaces=ns):
+                href = file_elem.attrib['href']
+                recursos.add(href)
+                files.add((identifier, href))
 
         # Obtener los identifierref de los items dentro de organization
         organization_items = root.findall(".//ims:organizations/ims:organization/ims:item", namespaces=ns)
         for item in organization_items:
             identifierref = item.attrib['identifierref']
-            if identifierref not in recursos:
-                print(f"Error: El identifierref '{identifierref}' no coincide con ningún identifier de recurso en imsmanifest.xml")
-                return False
+            for identifier, href in files:
+                if identifierref == identifier:
+                    recursos.add(href)
 
         #print("recursos:", recursos)
         #print("organization_items:", organization_items)
         print("La validación de imsmanifest.xml se ha completado correctamente.")
-        return True
+        return recursos
 
     except Exception as e:
         print(f"Error al analizar el archivo imsmanifest.xml: {e}")
-        return False
-
+        return set()
 
 if __name__ == "__main__":
     # Buscar imsmanifest.xml en el directorio actual
@@ -74,21 +89,16 @@ if __name__ == "__main__":
             print("El proyecto SCORM no está completo o tiene errores.")
         else:
             # Validar imsmanifest.xml
-            if not validar_imsmanifest(ruta_imsmanifest):
+            recursos = validar_imsmanifest(ruta_imsmanifest)
+            if not recursos:
                 print("El proyecto SCORM no está completo o tiene errores.")
             else:
                 # Recopilar todos los archivos en el proyecto
-                recursos = set()
-                tree = ET.parse(ruta_imsmanifest)
-                root = tree.getroot()
-                for resource in root.findall(".//{http://www.imsglobal.org/xsd/imsmd_rootv1p2p1}resource"):
-                    recursos.add(resource.attrib['identifier'])
-
                 archivos, archivos_no_utilizados = recopilar_archivos(ruta_actual, recursos)
                 
                 print("Archivos encontrados en el proyecto:")
-                for archivo in archivos:
-                    print(archivo)
+                #for archivo in archivos:
+                    #print(archivo)
 
                 if archivos_no_utilizados:
                     print("\nWarnings:")
