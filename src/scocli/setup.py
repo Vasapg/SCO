6feffc077f5c29a3483b7625a8870b9d664eb399
Victor Sanz Ararat <victoralejandro.sanz@alumnos.upm.es>
2024-05-18 16:18:04 +0200

    update build for new commands

--- src/scocli/setup.py
+++ src/scocli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scocli',
-    version='0.1',
+    version='1.0',
     author= 'Vasapg',
     description= 'CLI to facilitate the creation and management of SCORM packages',
     url='https://github.com/Vasapg/SCO/tree/main',
